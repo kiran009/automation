eb130b4498742c5d695fa9d6b9295f603903132a
Kiran Kumar D <kiran.daadhi@evolving.com>
2015-06-22 14:39:50 +0100

    Duplicate e-mail notification

--- Tertio_Automation.pl
+++ Tertio_Automation.pl
@@ -52,7 +52,7 @@ sub main()
 	#reconfigure_dev_proj_and_compile(); 
 	reconfigure_del_project();
 	delivery();
-	send_email();
+	#send_email();
 	#create_childcrs();
 	#move_cr_status();
 	ccm_stop();
