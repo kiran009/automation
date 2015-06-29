ba1e89c309db95afa99f0f4c0403cb62044275c6
Kiran Kumar D <kiran.daadhi@evolving.com>
2015-06-29 11:59:31 +0530

    Adding files

--- Tertio7.5.pl
+++ Tertio7.5.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# Tertio 7.7 Build Script
+# Tertio 7.5 Build Script
 use Cwd;
 use File::Path;
 use File::Find;
@@ -14,10 +14,10 @@ $ENV{'PATH'}="$ENV{'CCM_HOME'}/bin:$ENV{'PATH'}";
 $CCM="$ENV{'CCM_HOME'}/bin/ccm";
 $database="/data/ccmdb/provident/";
 $dbbmloc="/data/ccmbm/provident/";
-$result=GetOptions("devproject=s"=>\$devprojectname,"delproject=s"=>\$delprojectname);
+$result=GetOptions("crnumber=s"=>\$crnumber,"devproject=s"=>\$devprojectname);
 if(!$result)
 {
-	print "Please provide devprojectname, delprojectname \n";
+	print "Please provide crnumber, devprojectname \n";
 	exit;
 }
 if(!$devprojectname)
@@ -45,19 +45,68 @@ my $mailto='kiran.daadhi@evolving.com';
 my %hash;
 my $readmeIssue;
 
+# 1. CR number as an input
+# 2. Identify its parent CR
+# 3. Fetch the task/tasks out of the CR found in (2)
+# 4. Affected binary list ( Ideally should be fetched from README for the CR, but I don’t see README updated for the current or its parent CR)
+# 5. Add the tasks to Prep Project( 5-9 should be repeated per platform )
+# 6. Reconfigure the Dev Project
+# 7. Do make(gmake clean all)
+# 8. Modify the README for the patch ?  
+   #              >>> What are the changes needed in README
+      #           update the superseded and pre-req section of readme . This can be done manually/run a script “updatePatchREADME.ksh” in /data/ccmbm/final_script/imdad_copy (Currently build scripts take care about this)
+# 9. Create the patch tar bundles 
+# 10. Changes to Platform.mk 
+# 11. Move patches to Release Area
+# 12. CR transition to Patch_Test
+# 13. Send e-mail to the stakeholders about patch's availability
+
+# Job1:
+
+# 1. CR number(fetch tasknumbers about PROV) =>Get the parent CR(fetch TOME tasks from the README)
+# 2. fetch the binary list
+
+# Provident_Dev-patch_linAS5_7.6.0
+# ccm rp -show all_tasks Provident_Dev-patch_linAS5_7.6.0:project:1
+# ccm query "project match 'Provident_Dev-patch_linAS5_7.6.0'" -f "%objectname %release"
+# Provident_Dev-patch_sol10_7.6.0
+# ccm query "(is_member_of('Provident_Dev-patch_sol10_7.6.0'))" > /tmp/list_objects_solaris.txt
+# ccm query "project match 'Provident_Dev-patch_linAS5_7.6.2'"  -f "%objectname %release"
+# ccm query "(is_member_of('Provident_Dev-patch_linAS5_7.6.2'))"
+# ccm query "(is_member_of('Provident_Dev))"
+# ccm create -t project 'Provident_Dev-patch_linAS5_7.6.2' -c 'Project Provident_Dev-patch_linAS5_7.6.2' -release 'Tertio/7.6.2' -task 10839 -purpose 'Patch Generation'
+# ccm query "(is_member_of('TMNI_NI~i4.0_int:project:dennt#1')"
+# ccm query -type task "is_associated_task_of('4610:problem:probtrac')" 
+#  ccm query "is_patch_dev_child_of( problem_number='125')" 
+#  ccm query "has_patch_dev_child(cvtype='problem' and problem_number='126')
+#  ccm query "is_associated_task_of(cvtype='problem' and problem_number='4601')"  
+#  ccm query "is_associated_object_of('10207:task:probtrac')"  
+
 # /* Global Environment Variables ******* /
 sub main()
 {
 	start_ccm();
-	#reconfigure_dev_proj_and_compile(); 
-	reconfigure_del_project();
-	delivery();
-	send_email();
+    fetch_task_info();
+	
+	#reconfigure_del_project();
+	#delivery();
+	#send_email();
 	#create_childcrs();
 	#move_cr_status();
 	ccm_stop();
 	exit;
 }
+sub fetch_task_info()
+{
+	#ccm query "is_associated_task_of(cvtype='problem' and problem_number='4601')" -u -f "%task_number"
+	#10777
+	$parentcr=`$CCM query "has_patch_dev_child(cvtype='problem' and problem_number='$crnumber')" -u -f %problem_number`;		
+    print "Parent CR is: $parentcr \n";
+    $parentcr=~ s/^\s+|\s+$//g;	
+	@tasknumbers=`$CCM query "is_associated_task_of(cvtype='problem' and problem_number='$parentcr')" -u -f "%task_number"`;
+	print "List of tasks in it's parent cr: @tasknumbers \n";
+}
+
 sub reconfigure_dev_proj_and_compile()
 {	
 	$ccmworkarea=`$CCM wa -show -recurse $devprojectname`;
