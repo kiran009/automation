7e16465403155a9475d0a637132c2a4d15f87f04
Kiran Kumar D <kiran.daadhi@evolving.com>
2015-07-13 18:24:50 +0530

    Changes to destination path
    Change-Id: Ia30c13dd7aff027ee4b8bb5df9dffdf1c2d829ed
    Signed-off-by: Kiran Kumar D <kiran.daadhi@evolving.com>

--- package.pl
+++ package.pl
@@ -67,13 +67,14 @@ my @formattsks;
 my @binarylist;
 my $dtformat="$year$months[$mon]$mday$hour$min";
 my 	@location;
-my $tertiodest="/u/kkdaadhi/Tertio_Dest";
+#my $tertiodest="/u/kkdaadhi/Tertio_Dest";
+my $tertiodest="/data/releases/tertio/7.6.0/patches";
 # /* Global Environment Variables ******* /
 sub main()
 {	
 		createReadme();
 		pkg();
-		createMail();				
+		#createMail();				
 }
 sub createReadme()
 {
